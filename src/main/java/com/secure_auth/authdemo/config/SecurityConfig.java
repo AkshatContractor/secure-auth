929ff1dc66224c5f0831eb89382ca6ec0985ffa0
Akshat Contractor <akshatcontractor@gmail.com>
2025-07-06 22:54:24 +0530

    initial project setup

---
+++ src/main/java/com/secure_auth/authdemo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.secure_auth.authdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+
+        return security.build();
+    }
+}
